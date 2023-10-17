847c5fab420293f622be8ceb947f70161eec1cd1
zsqrq <wz527193449@163.com>
2023-10-17 12:48:49 +0800

    test

--- scripts/fastdds-gen.sh
+++ scripts/fastdds-gen.sh
@@ -1,4 +1,5 @@
 #！/bin/bash
+
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PATH=$script_path/../thirdparty/Fast-DDS-Gen/scripts:$PATH
 echo -e "\033[36m Start Building Project at time : $script_path \033[0m \n"
