7ad15bd6548f8715925e786088dff9f02534b614
zsqrq <wz527193449@163.com>
2024-01-11 18:57:06 +0800

    add pxsem codes example

--- example/unix_network/example/pxsem/semgetvalue.cpp
+++ example/unix_network/example/pxsem/semgetvalue.cpp
@@ -5,13 +5,16 @@
 
 int main(int argc, char** argv) {
   sem_t *sem;
-  int val;
+  int val, flags;
+  flags = O_RDWR | O_CREAT;
+  val = 0;
+
   if (argc != 2) {
     err_quit("usage: semgetvalue <name>");
   }
   sem = Sem_open(argv[1], 0);
   Sem_getvalue(sem, &val);
   printf("value = %d\n", val);
-
+  Sem_close(sem);
   exit(0);
 }
---
+++ example/unix_network/example/pxsem/sempost.cpp
@@ -0,0 +1,20 @@
+//
+// Created by wz on 24-1-11.
+//
+#include	"common/unpipc.h"
+
+int main(int argc, char **argv)
+{
+  sem_t	*sem;
+  int		val;
+
+  if (argc != 2)
+    err_quit("usage: sempost <name>");
+
+  sem = Sem_open(argv[1], 0);
+  Sem_post(sem);
+  Sem_getvalue(sem, &val);
+  printf("value = %d\n", val);
+
+  exit(0);
+}
